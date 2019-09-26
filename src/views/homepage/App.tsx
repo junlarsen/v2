f65f2d6b6b694dc08f3ba6bb9ade2fd600fc6fd5
supergrecko <me@supergrecko.com>
2019-09-26 22:03:35 +0200

    content: Update content

--- src/views/homepage/App.tsx
+++ src/views/homepage/App.tsx
@@ -46,7 +46,7 @@ export const App = (): JSX.Element => (
                 <Filler height={24} />
 
                 <Styled.Title>ABOUT ME</Styled.Title>
-                <Styled.Text>Second year high school student, self taught programmer since 2017.</Styled.Text>
+                <Styled.Text>High school student, programming hobbyist.</Styled.Text>
 
                 <Filler height={40} />
 
@@ -63,21 +63,21 @@ export const App = (): JSX.Element => (
                 <Filler height={12} />
 
                 <Link url="https://github.com/supergrecko/RiotQuest">RiotQuest</Link>
-                <Styled.Text>I enjoy messing with the Riot Games API so I built an open-source wrapper</Styled.Text>
+                <Styled.Text>I enjoy messing with the Riot Games API so I built an open-source wrapper in PHP</Styled.Text>
 
                 <Filler height={24} />
 
-                <Link url="https://github.com/supergrecko/tasky">Tasky</Link>
-                <Styled.Text>An extremely overengineered wrapper for your trusty terminal.</Styled.Text>
+                <Link url="https://github.com/supergrecko/images">Images</Link>
+                <Styled.Text>I needed somewhere to store screenshots so I wrote a Golang microservice to receive ShareX images.</Styled.Text>
 
                 <Filler height={24} />
 
-                <Link url="https://github.com/supergrecko/superlang">LeagueConnect</Link>
+                <Link url="https://github.com/supergrecko/league-connect">LeagueConnect</Link>
                 <Styled.Text>A basic node module for interacting with the League Client APIs</Styled.Text>
 
                 <Filler height={24} />
 
-                <Link url="https://github.com/supergrecko/v2">V2</Link>
+                <Link url="https://github.com/supergrecko/v2">Portfolio</Link>
                 <Styled.Text>This website, written with TypeScript, React and styled-components.</Styled.Text>
             </Slice>
         </Content>
